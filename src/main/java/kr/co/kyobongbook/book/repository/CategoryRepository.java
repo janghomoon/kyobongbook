074d3a72891b2b6345ee76e491f2bef66fb63a1d
jhmoon <jhmoon@howser.co.kr>
2024-08-10 12:04:49 +0900

    카테고리 repository수정

--- src/main/java/kr/co/kyobongbook/book/repository/CategoryRepository.java
+++ src/main/java/kr/co/kyobongbook/book/repository/CategoryRepository.java
@@ -3,6 +3,6 @@ package kr.co.kyobongbook.book.repository;
 import kr.co.kyobongbook.book.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriesRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
